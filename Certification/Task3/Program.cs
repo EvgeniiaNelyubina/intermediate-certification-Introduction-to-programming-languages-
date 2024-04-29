ebff554c6dfc4fa22b4a50947ebafc4cdd5ba2c3
Nelyubina Evgeniia <klimova.eug@yandex.ru>
2024-04-29 11:21:58 +0300

    Add Task3

---
+++ Certification/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+using System;
+
+class Program
+{
+    static Random random = new Random();
+
+    static void PrintReverse(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(array[index] + " ");
+            PrintReverse(array, index - 1);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Создаем массив случайных чисел
+        int[] array = new int[10];
+        Console.WriteLine("Исходный массив случайных чисел:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1, 101); // случайные числа от 1 до 100
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Элементы массива в обратном порядке:");
+        PrintReverse(array, array.Length - 1);
+    }
+}
